877d1ea58f9ad38ebabdbed6e09973b3d85bb911
Tringer Tamás <tamas.tringer@lechnerkozpont.hu>
2025-06-06 10:27:17 +0200

    Init repo

---
+++ PdfSignabilityChecker.cs
@@ -0,0 +1,39 @@
+﻿using iText.Kernel.Pdf;
+
+namespace PdfSignabilityCheckerTool;
+
+internal static class PdfSignabilityChecker
+{
+    internal static bool IsSignable(MemoryStream ms)
+    {
+        using PdfReader reader = new(ms);
+        using PdfDocument pdfDoc = new(reader);
+        PdfReader readerWithPermissions = pdfDoc.GetReader();
+        bool isEncrypted = readerWithPermissions.IsEncrypted();
+
+        if (!isEncrypted)
+            return true;
+
+        if (!CanCreateSignatureFieldDespiteEncryption(readerWithPermissions))
+        {
+            throw new InvalidOperationException("A PDF-ben le van tiltva a tartalom módosítása és/vagy az interaktív mezők létrehozása, az aláírásmező létrehozását is beleértve.");
+        }
+
+        return true;
+    }
+
+    private static bool CanCreateSignatureFieldDespiteEncryption(PdfReader readerWithPermissions)
+    {
+        int rawPermissions = readerWithPermissions.GetPermissions();
+        uint permissions = unchecked((uint)rawPermissions);
+        bool isContentModificationsAllowed = HasPermission(permissions, EncryptionConstants.ALLOW_MODIFY_CONTENTS);
+        bool isAnnotationModificationsAllowed = HasPermission(permissions, EncryptionConstants.ALLOW_MODIFY_ANNOTATIONS);
+
+        return isContentModificationsAllowed && isAnnotationModificationsAllowed;
+    }
+
+    private static bool HasPermission(uint permissions, int permissionFlag)
+    {
+        return (permissions & permissionFlag) != 0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PdfSignabilityCheckerTool;
+
+internal static class Program
+{
+    static int Main()
+    {
+        if (!Console.IsInputRedirected)
+        {
+            Console.Error.WriteLine("No input was redirected into the program.");
+            Environment.Exit(1);
+        }
+
+        using Stream stdin = Console.OpenStandardInput();
+        bool isSignable;
+
+        using (var ms = new MemoryStream())
+        {
+            stdin.CopyTo(ms);
+            ms.Position = 0;
+
+            try
+            {
+                isSignable = PdfSignabilityChecker.IsSignable(ms);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine("false");
+                return 99;
+            }
+        }
+
+        Console.WriteLine(isSignable
+            ? "true"
+            : "false");
+
+        return isSignable ? 0 : 3;
+    }
+}
